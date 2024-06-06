a56398cefeaef957c9ce77b19311bdbbe790ad79
tonkats <49250987+tonkats@users.noreply.github.com>
2024-06-06 10:05:51 +0200

    basic site configs

--- site.config.js
+++ site.config.js
@@ -1,30 +1,30 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "Max Bergfelt",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
+    role: "Machine Learning / Computer Vision Engineer",
     bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
+    email: "maxbergfelt@gmail.com",
     linkedin: "morethanmin",
-    github: "morethanmin",
+    github: "tonkats",
     instagram: "",
   },
   projects: [
-    {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
-    },
+    //{
+    //  name: `morethan-log`,
+    //  href: "https://github.com/morethanmin/morethan-log",
+    //},
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "max-blog",
+    description: "Welcome to my site!",
   },
 
   // CONFIG configration (required)
   link: "https://morethan-log.vercel.app",
-  since: 2022, // If leave this empty, current year will be used.
+  since: 2024, // If leave this empty, current year will be used.
   lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
 
